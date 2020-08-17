ddb492ce776a0bb0a3e27b3e3fc534dc1073726e
msitu@ceresimaging.net <60153807+msitu@users.noreply.github.com>
2020-08-17 13:19:58 -0700

    rough proto working

--- react/react-widget/src/widget.tsx
+++ react/react-widget/src/widget.tsx
@@ -1,6 +1,7 @@
 import { ReactWidget } from '@jupyterlab/apputils';
-
 import React from 'react';
+// import {render} from 'react-dom'
+import { Map, TileLayer } from 'react-leaflet';
 
 // import React, { useState } from 'react';
 
@@ -26,47 +27,105 @@ import React from 'react';
 //   );
 // };
 
-type FilePathProps = {
-  value: string;
-};
+// type FilePathProps = {
+//   value: string;
+// };
 
-type FilePathState = {
-  value?: string;
+// type FilePathState = {
+//   filepath?: string;
+// };
+// class FilePathForm extends React.Component<{}, FilePathState> {
+//   constructor(props: FilePathProps) {
+//     super(props);
+//     this.state = { filepath: '' };
+
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event: React.FormEvent<HTMLInputElement>) {
+//     this.setState({ filepath: event.currentTarget.value });
+//   }
+
+//   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+//     alert('Filepath has change! ' + this.state.filepath);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Filepath:
+//           <input
+//             type="text"
+//             value={this.state.filepath}
+//             onChange={this.handleChange}
+//           />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
+
+type LeafMapProps = {
+  filepath: String;
 };
-class FilePathForm extends React.Component<{}, FilePathState> {
-  constructor(props: FilePathProps) {
+type LeafMapState = {
+  center?: any;
+};
+class LeafMap extends React.Component<LeafMapProps, LeafMapState> {
+  constructor(props: LeafMapProps) {
     super(props);
-    this.state = { value: '' };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      center: null
+    };
+    this.fetchCenter();
   }
 
-  handleChange(event: React.FormEvent<HTMLInputElement>) {
-    this.setState({ value: event.currentTarget.value });
-  }
+  static defaultProps = {
+    filepath:
+      'Flight%208658/registered/2020-06-20%202378%20Red%20Hills%20D%20VNIR.tif'
+  };
 
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    alert('Filepath has change! ' + this.state.value);
-    event.preventDefault();
+  async fetchCenter() {
+    const resp = await fetch(
+      `http://192.168.64.20:32035/ceresflights/center/${this.props.filepath}`
+    );
+    const center = await resp.json();
+    console.log(center);
+    this.setState({ center });
   }
 
   render() {
+    if (!this.state.center) {
+      return <div></div>;
+    }
+
+    const url = `http://192.168.64.20:32035/ceresflights/tiles/{z}/{x}/{y}/${this.props.filepath}`;
+    // const url = "localhost"
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Filepath:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <Map center={this.state.center} zoom={18}>
+          <TileLayer url={url} />
+        </Map>
+      </div>
     );
   }
 }
+// const position = [51.505, -0.09]
+// const map = (
+//   <Map center={position} zoom={13}>
+//     <TileLayer
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+//     />
+//     <Marker position={position}>
+//       <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+//     </Marker>
+//   </Map>
+// )
 
 /**
  * A Counter Lumino Widget that wraps a CounterComponent.
@@ -81,6 +140,15 @@ export class CounterWidget extends ReactWidget {
   }
 
   render(): JSX.Element {
-    return <FilePathForm />;
+    return (
+      <div>
+        {/* <FilePathForm /> */}
+        <LeafMap />
+      </div>
+    );
+    // return render(
+    //   <LeafMap />,
+    //   document.querySelector('leaflet-container')
+    // )
   }
 }
