33b2c5126d323d37a783130c41135fbc2fb5cd31
msitu@ceresimaging.net <60153807+msitu@users.noreply.github.com>
2020-08-17 10:09:13 -0700

    add simple form widget

--- react/react-widget/src/widget.tsx
+++ react/react-widget/src/widget.tsx
@@ -1,28 +1,72 @@
 import { ReactWidget } from '@jupyterlab/apputils';
 
-import React, { useState } from 'react';
+import React from 'react';
 
-/**
- * React component for a counter.
- *
- * @returns The React component
- */
-const CounterComponent = (): JSX.Element => {
-  const [counter, setCounter] = useState(0);
+// import React, { useState } from 'react';
 
-  return (
-    <div>
-      <p>You clicked {counter} times!</p>
-      <button
-        onClick={(): void => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increment
-      </button>
-    </div>
-  );
+// /**
+//  * React component for a counter.
+//  *
+//  * @returns The React component
+//  */
+// const CounterComponent = (): JSX.Element => {
+//   const [counter, setCounter] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {counter} times!</p>
+//       <button
+//         onClick={(): void => {
+//           setCounter(counter + 1);
+//         }}
+//       >
+//         Increment
+//       </button>
+//     </div>
+//   );
+// };
+
+type FilePathProps = {
+  value: string;
 };
+
+type FilePathState = {
+  value?: string;
+};
+class FilePathForm extends React.Component<{}, FilePathState> {
+  constructor(props: FilePathProps) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event: React.FormEvent<HTMLInputElement>) {
+    this.setState({ value: event.currentTarget.value });
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    alert('Filepath has change! ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Filepath:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
 
 /**
  * A Counter Lumino Widget that wraps a CounterComponent.
@@ -37,6 +81,6 @@ export class CounterWidget extends ReactWidget {
   }
 
   render(): JSX.Element {
-    return <CounterComponent />;
+    return <FilePathForm />;
   }
 }
